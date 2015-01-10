7552f29406e8a9bbd0f49b865b7d24396bd717fd
kittleik <haavarki@stud.ntnu.no>
2015-01-10 18:11:39 +0800

    adding gameScreen

--- gen/ntust/finaleproject/picturegameproject/R.java
+++ gen/ntust/finaleproject/picturegameproject/R.java
@@ -1182,8 +1182,9 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_home_screen=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_game_screen=0x7f030018;
+        public static final int activity_home_screen=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
