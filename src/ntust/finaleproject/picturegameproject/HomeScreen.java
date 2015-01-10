2e7c3a11f8fd26d7c4cc10b15b96f34b8e1d17dd
kittleik <haavarki@stud.ntnu.no>
2015-01-10 17:40:48 +0800

    adding project

---
+++ src/ntust/finaleproject/picturegameproject/HomeScreen.java
@@ -0,0 +1,15 @@
+package ntust.finaleproject.picturegameproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class HomeScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home_screen);
+	}
+}
