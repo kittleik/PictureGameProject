c209cb579a866b5b36f06db7095caea613a5f29f
kittleik <haavarki@stud.ntnu.no>
2015-01-11 20:01:14 +0800

    cleanup

--- gen/ntust/finaleproject/picturegameproject/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package ntust.finaleproject.picturegameproject;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
