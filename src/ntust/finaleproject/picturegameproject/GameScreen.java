4701633613fde3a687ef55d69a00c14abdf815de
ChaikaChen <chuei1992@gmail.com>
2015-01-11 18:42:38 +0800

    Revert "setting up the game"
    This reverts commit 3a7f7957a4ea519eed631312d94ed21e826e687e.

--- src/ntust/finaleproject/picturegameproject/GameScreen.java
+++ src/ntust/finaleproject/picturegameproject/GameScreen.java
@@ -1,41 +1,15 @@
 package ntust.finaleproject.picturegameproject;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.ImageView;
-
 
 public class GameScreen extends Activity {
-	ImageView imageview, b1,b2,b3,b4,b5,b6,b7,b8,b9;
-	Button removebtn;
-	ArrayList<ImageView> grid = new ArrayList<ImageView>();
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home_screen);
-		imageview = (ImageView) findViewById(R.id.imageview);
-		b1 = (ImageView) findViewById(R.id.black1);
-		b2 = (ImageView) findViewById(R.id.black2);
-		b3 = (ImageView) findViewById(R.id.black3);
-		b4 = (ImageView) findViewById(R.id.black4);
-		b5 = (ImageView) findViewById(R.id.black5);
-		b6 = (ImageView) findViewById(R.id.black6);
-		b7 = (ImageView) findViewById(R.id.black7);
-		b8 = (ImageView) findViewById(R.id.black8);
-		b9 = (ImageView) findViewById(R.id.black9);
-		
-		removebtn = (Button) findViewById(R.id.removebtn);
-		
-		
 	}
-	private void showPiece(){
-		
-	}
-	
 }
